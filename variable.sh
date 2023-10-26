6e0d6db012ee907fcab34881d653c77beb7aa706
whitneybantar <misschal200@gmail.com>
2023-10-26 18:14:07 -0400

    Beginner Scripts to automate simple task

--- variable.sh
+++ variable.sh
@@ -2,6 +2,9 @@
 #author: Whitney B
 #Date: 10/26/2023
 
+#This is a simple script to store a variable which in this case is name.
+#A variable is simply used to store values that you can call many times in your script.
+
 Name="Whitney"
 echo "$Name can you please give us a smile?"
 sleep 2
